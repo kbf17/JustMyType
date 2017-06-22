b444b7da14bbde66c99e77e691dc1de88322223e
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-06-22 17:03:43 -0500

    target letter appears

--- styles.js
+++ styles.js
@@ -1,13 +1,24 @@
-$("body").keydown(function(key){
-    if (key.which == 16){
-        $("#keyboard-lower-container").hide();
-        $('#keyboard-upper-container').show();
-    }
-});
+var keyStroke;
+$(document).ready(function(){
 
-$("body").keyup(function(key){
-    if (key.which == 16){
-        $("#keyboard-lower-container").show();
-        $('#keyboard-upper-container').hide();
-    }
+    $("body").on('keydown', function(key){
+        if (key.which == 16){
+            $("#keyboard-lower-container").hide();
+            $('#keyboard-upper-container').show();
+        }
+    });
+
+    $("body").keyup(function(key){
+        if (key.which == 16){
+            $("#keyboard-lower-container").show();
+            $('#keyboard-upper-container').hide();
+        }
+    });
+
+    $("body").keypress(function(which){
+        keyStroke = String.fromCharCode(event.which);
+        console.log(keyStroke);
+        $('#target-letter').text(keyStroke);
+    })
+
 });
